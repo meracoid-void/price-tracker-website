00df043186f4a5ead7c5dab52eb5f1449dc96ccc
John Creech <johnwaynecreech1@gmail.com>
2026-03-21 16:45:30 -0400

    Additional API Log

--- netlify/functions/api-handler.js
+++ netlify/functions/api-handler.js
@@ -16,6 +16,7 @@ pool.on('error', (err) => {
 
 async function query(text, params) {
   try {
+    console.log('DB', process.env.DATABASE_URL);
     const res = await pool.query(text, params);
     return res;
   } catch (err) {
